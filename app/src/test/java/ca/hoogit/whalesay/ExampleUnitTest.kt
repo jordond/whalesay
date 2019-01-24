a8818850ac7696985a7690692cc48307453f90ec
Jordon de Hoog <jordon.dehoog@roomroster.com>
2019-01-24 13:21:26 -0500

    add deps

--- app/src/test/java/ca/hoogit/whalesay/ExampleUnitTest.kt
+++ app/src/test/java/ca/hoogit/whalesay/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package ca.hoogit.whalesay
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
