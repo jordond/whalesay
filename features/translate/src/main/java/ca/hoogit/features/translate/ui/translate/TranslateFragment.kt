a31295963a5aa02d4cc9189b892d9abc39c33e2c
Jordon de Hoog <jordon.dehoog@roomroster.com>
2019-01-26 17:15:29 -0500

    add a text bubble

--- features/translate/src/main/java/ca/hoogit/features/translate/ui/translate/TranslateFragment.kt
+++ features/translate/src/main/java/ca/hoogit/features/translate/ui/translate/TranslateFragment.kt
@@ -6,18 +6,17 @@ import ca.hoogit.features.translate.databinding.FragmentTranslateBinding
 import ca.hoogit.features.translate.ui.error.ErrorNavArgs
 import ca.hoogit.features.translate.ui.error.model.ErrorType
 import ca.hoogit.ktx.navigateTo
-import ca.hoogit.ktx.onClick
 
 class TranslateFragment : BindableFragment<FragmentTranslateBinding>() {
 
     override fun getLayoutRes(): Int = R.layout.fragment_translate
 
     override fun setupViews() = with(binding) {
-        txtTest.onClick { }
-
-        btnTestGeneric.onClick { showErrorScreen(ErrorType.Generic) }
-        btnTestGoogle.onClick { showErrorScreen(ErrorType.Google) }
-        btnTestNetwork.onClick { showErrorScreen(ErrorType.Network) }
+//        txtTest.onClick { }
+//
+//        btnTestGeneric.onClick { showErrorScreen(ErrorType.Generic) }
+//        btnTestGoogle.onClick { showErrorScreen(ErrorType.Google) }
+//        btnTestNetwork.onClick { showErrorScreen(ErrorType.Network) }
     }
 
     private fun showErrorScreen(error: ErrorType) {
